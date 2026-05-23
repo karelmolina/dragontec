9087d357c8b3112ec650ad7a7050214e2d5f254e
Molina, Karel <karelmzuniga@gmail.com>
2026-05-22 19:56:08 -0600

    feat: initialize project

---
+++ test/widget_test.dart
@@ -0,0 +1,18 @@
+import 'package:dragontec/app.dart';
+import 'package:dragontec/injection_container.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  setUpAll(() async {
+    await initDependencies();
+  });
+
+  testWidgets('App renders splash screen', (WidgetTester tester) async {
+    await tester.pumpWidget(const DragontecApp());
+
+    // Verify that the splash screen shows the app name.
+    expect(find.text('Horus Logistic'), findsOneWidget);
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+}
